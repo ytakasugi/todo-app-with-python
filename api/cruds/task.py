8939cb52d22cf5f4b71a7136a6feb601bb5f7f70
ytakasugi <sh7.tibi0129@gmail.com>
2022-03-27 19:38:42 +0900

    change file

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,4 +1,7 @@
+from typing import List, Tuple, Optional
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
@@ -12,3 +15,38 @@ async def create_task(
     await db.commit()
     await db.refresh(task)
     return task
+
+async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
+    result: Result = await (
+        db.execute(
+            select(
+                task_model.Task.id,
+                task_model.Task.title,
+                task_model.Done.id.is_not(None).label("done"),
+            ).outerjoin(task_model.Done)
+        )
+    )
+    return result.all()
+
+async def get_task(db: AsyncSession, task_id: int) -> Optional[task_model.Task]:
+    result: Result = await db.execute(
+        select(task_model.Task).filter(task_model.Task.id == task_id)
+    )
+    task: Optional[Tuple[task_model.Task]] = result.first()
+    # 要素が一つであってもtupleで返却されるので１つ目の要素を取り出す
+    return task[0] if task is not None else None
+
+async def update_task(
+    db: AsyncSession,
+    task_create: task_schema.TaskCreate,
+    original: task_model.Task
+) -> task_model.Task:
+    original.title = task_create.title
+    db.add(original)
+    await db.commit()
+    await db.refresh(original)
+    return original
+
+async def delete_task(db: AsyncSession, original: task_model.Task) -> None:
+    await db.delete(original)
+    await db.commit()
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,18 +1,19 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-import api.cruds.task as task_crud
 from api.db import get_db
-
+import api.cruds.task as task_crud
+import api.cruds.done as done_crud
+import api.schemas.done as done_schema
 import api.schemas.task as task_schema
 
 router = APIRouter()
 
 # Taskクラスを複数返すので、Listとして定義
 @router.get("/tasks", response_model=List[task_schema.Task])
-async def list_task():
-    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(
@@ -22,9 +23,36 @@ async def create_task(
     return await task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+async def update_task(
+    task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.update_task(db, task_body, original=task)
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int):
-    return
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.delete_task(db, original=task)
+
+@router.put("/tasks/{task_id}/done", response_model=done_schema.DoneResponse)
+async def mark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
+    done = await done_crud.get_done(db, task_id=task_id)
+    if done is not None:
+        raise HTTPException(status_code=400, detail="Done already exists")
+
+    return await done_crud.create_done(db, task_id)
+
+
+@router.delete("/tasks/{task_id}/done", response_model=None)
+async def unmark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
+    done = await done_crud.get_done(db, task_id=task_id)
+    if done is None:
+        raise HTTPException(status_code=404, detail="Done not found")
+
+    return await done_crud.delete_done(db, original=done)
